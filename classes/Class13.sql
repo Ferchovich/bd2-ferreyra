8c20080a4a9e01d89679505c594d6ad95de1fd3e
Ferchovich <ferreyraoctavio6@gmail.com>
2024-08-19 09:28:18 -0300

    Class 13 Finished

--- classes/Class13.sql
+++ classes/Class13.sql
@@ -1,39 +1,6 @@
-/*
-Write the statements with all the needed subqueries, do not use hard-coded ids unless is specified. Check which fields are mandatory and which ones can be ommited (use default value).
 
-Add a new customer
-
-To store 1
-For address use an existing address. The one that has the biggest address_id in 'United States'
-Add a rental
-
-Make easy to select any film title. I.e. I should be able to put 'film tile' in the where, and not the id.
-Do not check if the film is already rented, just use any from the inventory, e.g. the one with highest id.
-Select any staff_id from Store 2.
-Update film year based on the rating
-
-For example if rating is 'G' release date will be '2001'
-You can choose the mapping between rating and year.
-Write as many statements are needed.
-Return a film
-
-Write the necessary statements and queries for the following steps.
-Find a film that was not yet returned. And use that rental id. Pick the latest that was rented for example.
-Use the id to return the film.
-Try to delete a film
-
-Check what happens, describe what to do.
-Write all the necessary delete statements to entirely remove the film from the DB.
-Rent a film
-
-Find an inventory id that is available for rent (available in store) pick any movie. Save this id somewhere.
-Add a rental entry
-Add a payment entry
-Use sub-queries for everything, except for the inventory id that can be used directly in the queries.
-Once you're done. Restore the database data using the populate script from class 3.
-
-*/
 use sakila;
+#1
 insert into customer (store_id, address_id, first_name, last_name, email)
 values (1,(
 	select max(address_id) from address ad
@@ -43,8 +10,8 @@ values (1,(
 ), "Octavio", "Ferreyra", "ferreyra@gmail.com");
 
 
-#ANNIE IDENTITY
 
+#2
 insert into rental (rental_date, inventory_id, staff_id, customer)
 values (now(),(
 	select inventory_id from inventory i 
@@ -53,6 +20,42 @@ values (now(),(
 ), (
 	select max(staff_id) from staff where store_id=2), 300);
     
-
+#3
 update film set release_year = '1990' where rating = "G"; 
-update film 
+update film SET release_year = '1991' WHERE rating = "PG";
+update film SET release_year = '1992' WHERE rating = "PG-13";
+update film SET release_year = '1994' WHERE rating = "R";
+
+#4
+update rental 
+set return_date=now()
+where rental_id=(select max(rental_id) where return_date is null);
+
+#5
+delete from film where film_id=1;
+delete from film_actor where film_id=1;
+delete from film_category where film_id=1;
+delete from rental where inventory_id in (select inventory_id from inventory where film_id=1);
+delete from inventory where film_id=1;
+delete from film where film_id=1;
+select * from film order by film_id asc;
+
+#6
+select inventory_id from inventory i left outer join rental r using(inventory_id) where r.rental_id is null limit 1;
+insert into rental (rental_date,inventory_id,customer_id,staff_id) values (
+	NOW(), 
+	(4587), 
+    (SELECT customer_id FROM customer LIMIT 1), 
+    (SELECT staff_id FROM staff LIMIT 1));
+    
+INSERT INTO payment (customer_id, staff_id, rental_id, amount, payment_date)
+VALUES (
+    (SELECT customer_id FROM rental WHERE inventory_id = 4587 LIMIT 1),
+    (SELECT staff_id FROM rental WHERE inventory_id = 4587 LIMIT 1),   
+    (SELECT max(rental_id) FROM rental WHERE inventory_id = 4587),  
+    466.43, 
+    NOW() 
+);
+
+
+
